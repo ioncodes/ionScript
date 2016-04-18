3b4fffec71ce632b9085a0b2264f5b583fa7b638
ion <ionix@protonmail.com>
2016-04-18 20:06:29 +0200

    out, in, set, sset, dset working :D script file added. check //bugfix lines for help (those 2 lines are just to get this version working) tested with "script.ion"

---
+++ Worker/Work.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worker
+{
+    public class Work
+    {
+        public static void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static string Read()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
